42c2e268c722781993baafd2bf085075f9f4813a
CN-12 <gamdragon2@gmail.com>
2022-04-19 05:14:20 +0900

    shoe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import React, { useState } from "react";
+import {
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Jumbotron,
+  Button,
+} from "react-bootstrap";
+import "./App.css";
+import Data from "./data.js";
 
 function App() {
+  let [shoes, shoes변경] = useState(Data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Jumbotron className="background">
+        <h1>20% Season Off</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
+
+      <div className="container">
+        <div className="row">
+          {/* <Card shoes={shoes[0]} />
+          <Card shoes={shoes[1]} />
+          <Card shoes={shoes[2]} /> */}
+          {shoes.map((a, i) => {
+            return <Card shoes={a} i={i} key={i}/>;
+          })}
+          {/* {
+            shoes.map(function(신발){
+              return  (
+              <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            <h4>{ 신발.title }</h4>
+            <p>{ 신발.content} & { 신발.price}</p>
+          </div>
+            )
+            })
+          } */}
+        </div>
+      </div>
     </div>
   );
 }
-
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src={ 'https://codingapple1.github.io/shop/shoes' + (props.i+1) + '.jpg' } width="100%" />
+      <h4> {props.shoes.title}</h4>
+      <p> {props.shoes.content} & {props.shoes.price} </p>
+    </div>
+  );
+}
 export default App;
