7667c2d1a556c8583a3b2566246818bdfa8e90b3
CN-12 <gamdragon2@gmail.com>
2022-04-19 09:41:02 +0900

    다시

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { Link, Route, Switch } from 'react-router-dom';
 import Detail from './Detail.js';
 
 function App() {
+
   let [shoes, shoes변경] = useState(Data);
 
   return (
@@ -66,8 +67,12 @@ function App() {
         </div>
       </Route>
 
+      <Route path="/detail/:id">
+        <Detail shoes={shoes}/>
+      </Route>
+      
       <Route path="/detail">
-        <Detail />
+        <Detail shoes={shoes} />
       </Route>
       {/* <Route path="/어쩌구" component={Modal}></Route> */}
       <Route path="/:id">
--- src/Detail.js
+++ src/Detail.js
@@ -1,23 +1,35 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
-function Detail() {
-  
+
+let 박스 = styled.div`
+  padding : 20px;
+`;
+let 제목 = styled.h4`
+  font-size : 25px;
+  color : ${ props => props.색상 }
+`;
+
+function Detail(props) {
+  let { id } = useParams();
   let history = useHistory();
-
+  let 찾은상품 = props.shoes.find(function(상품){
+    return 상품.id == id
+  });
   return (
     <div className="container">
+      <박스>
+        <제목 색상='red'>Detail</제목>
+      </박스>
       <div className="row">
         <div className="col-md-6">
-          <img
-            src="https://codingapple1.github.io/shop/shoes1.jpg"
-            width="100%"
-          />
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>
         </div>
         <div className="col-md-6 mt-4">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}</p>
           <button className="btn btn-danger">주문하기</button>
           <button className="btn btn-danger" onClick={()=>{ history.goBack()}}>뒤로가기</button>
         </div>
